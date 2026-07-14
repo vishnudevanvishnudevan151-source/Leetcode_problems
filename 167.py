25430b6b76bb8a612bb15d460693efbfeb4870ae
vishnudevanvishnudevan151-source <vishnudevanvishnudevan151@gmail.com>
2026-07-14 17:26:01 +0530

    Add files via upload

---
+++ 167.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+
+            if current_sum == target:
+                return [left + 1, right + 1]   # 1-based indexing
+
+            elif current_sum < target:
+                left += 1
+
+            else:
+                right -= 1
