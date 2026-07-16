5f34249ced3916809a3dbd0ea7f1454a17885280
vishnudevanvishnudevan151-source <vishnudevanvishnudevan151@gmail.com>
2026-07-16 15:34:21 +0530

    Add files via upload

---
+++ 206.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        
+        while curr:
+            next_node = curr.next  
+            curr.next = prev       
+            prev = curr            
+            curr = next_node      
+            
+        return prev  
