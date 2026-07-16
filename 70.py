61c024a45fb97e32419b04616f6c9f0ec87e1d28
vishnudevanvishnudevan151-source <vishnudevanvishnudevan151@gmail.com>
2026-07-16 14:56:56 +0530

    Add files via upload

---
+++ 70.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        
+        a, b = 1, 2
+        
+        for _ in range(3, n + 1):
+            current = a + b
+            a = b
+            b = current
+            
+        return b
